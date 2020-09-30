d3e3420f06d3b7b1d764261300704e781787b00c
Martins Bormanis <martins.bormanis@gmail.com>
2020-09-30 10:37:23 +0300

    Add alternative for loop

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
 function accordize() {
     let heading = document.getElementsByClassName("heading")
 
-    for (let i = 0; i<heading.length; i++) {
-        heading[i].addEventListener("click", function() {
-            for (let i2 of heading) { // šeit gan mēģināju arī for (let i2 = 0; i2<heading.length; i2++), bet īsti nesaprotu kāpēc nesanāca.
-                if (i2 != this) {
-                    i2.classList.remove("open");
+    for (let i = 0; i < heading.length; i++) {
+        heading[i].addEventListener("click", function () {
+
+            for (let i2 = 0; i2 < heading.length; i2++) {
+                if (heading[i2] !== this) {
+                    heading[i2].classList.remove("open");
                 } else {
-                    i2.classList.toggle("open");
+                    heading[i2].classList.toggle("open");
                 }
             }
         });
